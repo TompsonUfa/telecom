000716bbd0498c093830ea0775620d37680a2a38
Roman <romanrus98@mail.ru>
2023-10-24 22:50:34 +0500

    Правка роутов на apiResource

--- routes/api.php
+++ routes/api.php
@@ -3,26 +3,10 @@
 use App\Http\Controllers\Api\EquipmentController;
 use Illuminate\Support\Facades\Route;
 
-//Route::middleware('auth:sanctum')->group(function (){
 
-    Route::prefix('equipment')->group(function (){
-
-        Route::get('/equipment', [EquipmentController::class, 'index']);
-
-        Route::get('', [EquipmentController::class, 'index']);
-
-        Route::get('/{id}', [EquipmentController::class, 'show']);
-
-        Route::post('', [EquipmentController::class, 'store']);
-
-        Route::put('/{id}', [EquipmentController::class, 'update']);
-
-        Route::delete('/{id}', [EquipmentController::class, 'destroy']);
-
-    });
+    Route::apiResource('equipment', EquipmentController::class);
 
     Route::get('equipment-type', [EquipmentController::class, 'type']);
 
-//});
 
 
